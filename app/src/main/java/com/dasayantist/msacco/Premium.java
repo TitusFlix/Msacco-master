ae9bc8fb35e502ed5714ff5f08b44ea9484627a1
TitusFlix <tituskibor12@gmail.com>
2019-05-08 12:05:56 +0300

    all

--- app/src/main/java/com/dasayantist/msacco/Premium.java
+++ app/src/main/java/com/dasayantist/msacco/Premium.java
@@ -252,7 +252,7 @@ public class Premium extends BaseActivity implements PaymentListener, IMainActiv
             }
         }
     }
-
+//
     private void setUp() {
         chowder = new Chowder(Premium.this, PAYBILL_NUMBER, PASSKEY, this);
         etLAmount = findViewById(R.id.etLAmount);
@@ -601,7 +601,7 @@ public class Premium extends BaseActivity implements PaymentListener, IMainActiv
 
                 return null;
             } catch (Exception e) {
-                String s = new String("Exception: " + e.getMessage());
+                String s = "Exception: " + e.getMessage();
                 System.out.print(s);
                 return null;
 
